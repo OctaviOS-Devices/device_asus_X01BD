a8b7da78605f03c62837b9b28acc9aa9ce7fd868
Arseniy Graur <argraur@gmail.com>
2020-01-24 14:08:14 +0000

    X01BD: Use Pixel Charging Animation

--- sdm660.mk
+++ sdm660.mk
@@ -256,6 +256,11 @@ PRODUCT_PACKAGES += \
     charger_res_images \
     product_charger_res_images
 
+# Offline charger
+PRODUCT_PACKAGES += \
+    charger_res_images \
+    product_charger_res_images
+
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
