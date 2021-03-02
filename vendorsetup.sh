6a6c6cfe053b45b1a03bf2645042d5541c869a4a
ElectroPerf <kunmun.devroms@gmail.com>
2021-03-02 16:35:36 +0000

    Compile with Proton 13 clang and track op addon repo

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,3 +7,5 @@ git clone https://github.com/LineageOS/android_hardware_qcom_media -b lineage-18
 git clone https://github.com/LineageOS/android_hardware_qcom_audio -b lineage-18.1-caf-msm8998 hardware/qcom-caf/msm8998/audio
 rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
+git clone https://gitlab.com/ElectroPerf/vendor_addons_oneplus_apps.git -b OOS11 vendor/addons/oneplus/apps
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
