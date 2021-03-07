46123f4b762882c2973ef139af729e8c9730edf7
ElectroPerf <kunmun.devroms@gmail.com>
2021-03-07 16:04:17 +0000

    Clone facelock

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -9,3 +9,4 @@ rm -rf vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr.git -b lineage-18.1 vendor/qcom/opensource/data-ipa-cfg-mgr
 git clone https://gitlab.com/ElectroPerf/vendor_addons_oneplus_apps.git -b OOS11 vendor/addons/oneplus/apps
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git -b master prebuilts/clang/host/linux-x86/clang-proton
+git clone https://bitbucket.org/syberia-project/external_motorola_faceunlock.git -b 11.0 external/motorola/faceunlock
